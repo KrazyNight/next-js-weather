c5220ba9535b23c21914c70f20e23f620fd38ca8
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-05-05 12:07:40 -0700

    begining part 4.5

--- app/page.tsx
+++ app/page.tsx
@@ -29,24 +29,16 @@ export default function Home() {
           />
         </div>
 
-        
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            <div className="lg:col-span-2 space-y-6">
-              <CurrentWeather  />
-
-              <Metrics />
-
-              <DailyForecast
-              
-              />
-            </div>
-
-            <div className="lg:col-span-1">
-              <HourlyForecast
-            
-              />
-            </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          <div className="lg:col-span-2 space-y-6">
+            <CurrentWeather  />
+            <Metrics />
+            <DailyForecast />
           </div>
+          <div className="lg:col-span-1">
+            <HourlyForecast />
+          </div>
+        </div>
       
       </div>
     </div>
