d47c9acc524072d9e1a489734f260c17e2850a60
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-05-05 12:03:34 -0700

    begining part 4

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,49 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <Header />
+    {/* <Header />
 
     <Search />
     <CurrentWeather />
     <Metrics />
     <DailyForecast />
-    <HourlyForecast />
+    <HourlyForecast /> */}
+
+    <div className="min-h-screen text-white">
+      <div className="container mx-auto px-4 py-6 max-w-7xl">
+        <Header />
+
+        <div className="text-center mb-8">
+          <h1 className="text-3xl md:text-5xl font-bold mb-16 text-shadow">
+            How's the sky looking today?
+          </h1>
+          <Search
+          />
+        </div>
+
+        
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-2 space-y-6">
+              <CurrentWeather  />
+
+              <Metrics />
+
+              <DailyForecast
+              
+              />
+            </div>
+
+            <div className="lg:col-span-1">
+              <HourlyForecast
+            
+              />
+            </div>
+          </div>
+      
+      </div>
+    </div>
+
+
     </>
   );
 }
