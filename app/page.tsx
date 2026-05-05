9ec738b69944c7db907fdd414da33ae663a7b9a0
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-05-05 11:43:21 -0700

    begining part 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import CurrentWeather from "@/components/CurrentWeather";
+import DailyForecast from "@/components/DailyForecast";
+import Header from "@/components/Header";
+import HourlyForecast from "@/components/HourlyForecast";
+import Metrics from "@/components/Metrics";
+import Search from "@/components/Search";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    App
-    let the coding begin!
+    <Header />
+
+    <Search />
+    <CurrentWeather />
+    <Metrics />
+    <DailyForecast />
+    <HourlyForecast />
     </>
   );
 }
